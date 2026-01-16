be1ca92d8fd5bf83835bed7995ec650083b2dd6f
toki1616 <tokigit@gmail.com>
2026-01-16 19:57:45 +0900

    Enemyを保存する処理の追加

---
+++ Assets/Project/Components/Save/Test/Scripts/Enemy/EnemyView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace My.Save
+{
+    public class EnemyView : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField idInputField;
+
+        [SerializeField]
+        private InputField nameInputField;
+
+        [SerializeField]
+        private InputField typeInputField;
+
+        public void SetEnemy(TestEnemy enemy)
+        {
+            idInputField.text = $"{enemy.Id}";
+            nameInputField.text = enemy.Name;
+            typeInputField.text = enemy.Type;
+        }
+
+        public TestEnemy GetEnemy()
+        {
+            return new TestEnemy(
+                int.Parse(idInputField.text),
+                nameInputField.text,
+                typeInputField.text
+            );
+        }
+    }
+}
